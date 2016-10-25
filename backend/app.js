9a0b10ff69a5bdd9f886de9e82a44d9a4fb2d48c
Alan Lee <alee@bluelasso.com>
2016-10-25 11:28:31 -0700

    basic mysql operation

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
 const server = require('./Server');
+const mysql = require('./Mysql');
+
 server.startServer();
+mysql.start();
 
--- backend/mysql.js
+++ backend/mysql.js
@@ -1,21 +1,46 @@
-"use strict";
+'use strict';
 
 const mysql = require('mysql');
 
-let Mysql = function(){
-	let connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'company'
-	});
-	
-	this.connect = connection.connect(function (err) {
-		if (!err) {
-			console.log("Database is connected ... \n\n");
-		} else {
-			console.log("Error connecting database ... \n\n");
-		}
-	});
+let Mysql = function () {
+    // const sql = this;
+
+    let conn = mysql.createConnection({
+        host: '0.0.0.0',
+        user: 'root',
+        password: '',
+        database: 'company',
+        port: '3306'
+    });
+
+    this.sendQuery = function ( query ){
+        console.log('send query : '+query);
+        
+        conn.query( query, function(err,rows){
+            if(err) {
+                console.log(err);
+                throw err;
+            }
+            console.log('Data received from Db:\n');
+            console.log(rows);
+        });
+    };
+
+    conn.connect(function (err) {
+        if (!err) {
+            console.log("Database is connected ... \n\n");
+        } else {
+            console.log("Error connecting database ... \n\n"+err);
+        }
+    });
 };
+
+Mysql.start = function(){
+    let mysql = new Mysql();
+    mysql.sendQuery( 'SELECT * FROM employees' );
+
+};
+
+module.exports = Mysql;
+
 
