f4099c85795b7e73b229085799d88743d6cc8921
AlanCILee <alan.changil.lee@gmail.com>
2016-11-11 20:09:22 -0800

    loginservice

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const express = require('express'),
         fs = require('fs'),
         EmployeeService = require('./services/EmployeeService'),
         DepartmentService = require('./services/DepartmentService'),
-        TimezoneService = require('./services/TimezoneService');
+        TimezoneService = require('./services/TimezoneService'),
+        LoginService = require('./services/LoginService');
 
 const Server = function(options) {
     const server = this;
@@ -19,6 +20,7 @@ const Server = function(options) {
         let employeeServiceObj = new EmployeeService(options),
             departmentServiceObj = new DepartmentService(options),
             timezoneServiceObj = new TimezoneService(options),
+            loginServiceObj = new LoginService(options),
             allowCrossDomain = function(req, res, next) {
             res.header('Access-Control-Allow-Origin', "*");
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -35,46 +37,9 @@ const Server = function(options) {
             cookie: { secure : false, httpOnly : false },
         }));
         app.use(allowCrossDomain);
-    
         
-        app.post('/login', function(req, res){
-            let userId = req.body.id;
-            let password = req.body.password;
-            console.log('post login req:',userId, ':', password);
-
-            let query = `SELECT * FROM managers  
-                INNER JOIN companies ON managers.companyId = companies.companyId 
-                WHERE name = "${userId}" AND password = "${password}"`;
-
-            let database = 'clients';
-
-            mysql.sendQuery( database, query, function(err, rows, fields){
-                if(err){
-                    console.log('sendQuery fail: ', err);
-                }else {
-                    console.log('', rows);
-                    if (rows.length > 0) {
-                        if(userId === rows[0].name && password === rows[0].password) {
-                            console.log('LOGIN START:', req.session);
-                            req.session.viewname = rows[0].viewname;
-                            req.session.company = rows[0].companyName;
-                            req.session.save();
-                            console.log('session: ', req.session);
-                            // res.redirect('/welcome');
-                        }
-                    }else{
-
-                    }
-                }
-                res.send(req.session);
-            });
-        });
-       
-        app.get('/logout', function(req, res){
-            console.log('user logout', req.session);
-            // delete req.session.viewname;
-            res.send(req.session);
-        });
+        app.post('/login', loginServiceObj.logIn);
+        app.get('/logout', loginServiceObj.logOff);
 
         app.post('/newDepartment', departmentServiceObj.newDepartmentDb);
         app.post('/upDepartment', departmentServiceObj.upDepartmentDb);
---
+++ backend/services/LoginService.js
@@ -0,0 +1,46 @@
+
+const LoginService = function(options) {
+	const mysql = options.mysql;
+	
+	this.logIn = function(req, res){
+		let userId = req.body.id;
+		let password = req.body.password;
+		console.log('post login req:',userId, ':', password);
+		
+		let query = `SELECT * FROM managers  
+                INNER JOIN companies ON managers.companyId = companies.companyId 
+                WHERE name = "${userId}" AND password = "${password}"`;
+		
+		let database = 'clients';
+		
+		mysql.sendQuery( database, query, function(err, rows, fields){
+			if(err){
+				console.log('sendQuery fail: ', err);
+			}else {
+				console.log('', rows);
+				if (rows.length > 0) {
+					if(userId === rows[0].name && password === rows[0].password) {
+						console.log('LOGIN START:', req.session);
+						req.session.viewname = rows[0].viewname;
+						req.session.company = rows[0].companyName;
+						req.session.save();
+						console.log('session: ', req.session);
+						// res.redirect('/welcome');
+					}
+				}else{
+					
+				}
+			}
+			res.send(req.session);
+		});
+	};
+	
+	this.logOff = function(req, res){
+		console.log('user logout', req.session);
+		// delete req.session.viewname;
+		res.send(req.session);
+	};
+	
+}
+
+module.exports = LoginService;
