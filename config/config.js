26781b47791ec2aef9b00e12c18cd04e39d0d68e
Alan Lee <alee@bluelasso.com>
2016-11-25 16:16:46 -0800

    latest config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 exports.DB_PSWD = '';
-exports.SERVER_PORT = 3000;
-// exports.SERVER = 'timercash.com';
-exports.SERVER = 'localhost';
+exports.SERVER_PORT = 80;
+exports.SERVER = 'timercash.com';
+// exports.SERVER_PORT = 3000;
+// exports.SERVER = 'localhost';
